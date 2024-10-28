6e74e15edec0724c2eaa141ab4a4eebfb484db61
DOF1109 <friasdaniel.1109@gmail.com>
2024-10-28 19:48:00 -0300

    Post from form in proccess

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,13 +1,50 @@
+"use client";
+
 const NewPage = () => {
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+
+    const res = await fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, description }),
+    }).then((resp) => resp.json);
+
+    console.log(res);
+  };
+
   return (
-    <div>
-      <form action="">
-        <input type="text" />
-        <textarea row="3"></textarea>
-        <button>Create</button>
+    <div className="h-screen flex justify-center items-center">
+      <form className="bg-slate-800 p-10" onSubmit={handleSubmit}>
+        <label htmlFor="title" className="font-bold text-sm">
+          Title
+        </label>
+        <input
+          id="title"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          type="text"
+          placeholder="Title"
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Description
+        </label>
+        <textarea
+          id="description"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          row="3"
+          placeholder="Description"
+        ></textarea>
+        <button className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
+          Create
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewPage
+export default NewPage;
