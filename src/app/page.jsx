7a89ccc97aa7e12577bb83b9f2a6d69359dc335c
DOF1109 <friasdaniel.1109@gmail.com>
2024-10-29 20:03:58 -0300

    List all tasks completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,29 @@
-const HomePage = () => {
-  return (
-    <div>Tasks</div>
-  )
-}
+const loadTasks = async () => {
+  const res = await fetch("http://localhost:3000/api/tasks").then((resp) =>
+    resp.json()
+  );
+  return res;
+};
 
-export default HomePage
+const HomePage = async () => {
+  const tasks = await loadTasks();
+
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+          <div
+            className="bg-slate-800 p-3 hover:bg-slate-700 hover:cursor-pointer"
+            key={task.id}
+          >
+            <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
+            <p>{task.description}</p>
+            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
